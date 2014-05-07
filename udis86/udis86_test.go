82b75e2c7ecac24cd9f68f86ddedd9488d8a036b
Nelson Elhage <nelhage@nelhage.com>
2014-05-07 13:31:19 -0700

    tests for syntax

--- udis86/udis86_test.go
+++ udis86/udis86_test.go
@@ -14,13 +14,12 @@ func TestBasic(t *testing.T) {
 	if d.Len() != 1 {
 		t.Errorf("Len() returned %d, not 1", d.Len())
 	}
-	bytes := d.Bytes()
-	if !reflect.DeepEqual(bytes, []byte{0x90}) {
+
+	if bytes := d.Bytes(); !reflect.DeepEqual(bytes, []byte{0x90}) {
 		t.Errorf("Bytes() returned %v, wanted %v", bytes, []byte{0x90})
 	}
 
-	m := d.Mnemonic()
-	if m != I_nop {
+	if m := d.Mnemonic(); m != I_nop {
 		t.Errorf("mnemonic was %d (%s), want I_nop!", m, m.String())
 	}
 }
@@ -36,5 +35,19 @@ func TestReader(t *testing.T) {
 	}
 	if d.Next() {
 		t.Errorf("Next() returned true on a second call!")
+	}
+}
+
+func TestSyntax(t *testing.T) {
+	d := New(&Config{Buf: []byte{0x48, 0xff, 0xc0}, Syntax: SyntaxATT, Bits: 64})
+	d.Next()
+	if asm := d.String(); asm != "inc %rax" {
+		t.Errorf("bad ATT-syntax disassembly: %s", asm)
+	}
+
+	d = New(&Config{Buf: []byte{0x48, 0xff, 0xc0}, Syntax: SyntaxIntel, Bits: 64})
+	d.Next()
+	if asm := d.String(); asm != "inc rax" {
+		t.Errorf("bad Intel-syntax disassembly: %s", asm)
 	}
 }
